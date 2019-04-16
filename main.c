ebdaeae3f739e964a57efb41d0cc3dfd73a5081f
Richard Lupton <richard.lupton@gmail.com>
2019-04-16 20:58:51 +0100

    Basic mode handling

--- main.c
+++ main.c
@@ -8,14 +8,12 @@
 #include "buffer.h"
 
 
-typedef int event_t;
-
-
-typedef struct editor_state_t {
+struct editor_state_t {
   buffer_iter_t *buffer;
   buffer_iter_t *point;
+  mode_t *mode;
   bool terminate;
-} editor_state_t;
+};
 
 
 /* Functions for working with editor state */
@@ -30,6 +28,18 @@ void render_modeline(const editor_state_t *const state);
 bool should_quit(const editor_state_t *const state);
 
 
+static mode_t normal_mode = (mode_t) {
+                                      .name = "NORMAL",
+                                      .handler = normal_mode_handler
+};
+
+
+static mode_t insert_mode = (mode_t) {
+                                      .name = "INSERT",
+                                      .handler = insert_mode_handler
+};
+
+
 int main(int argv, char *argc[])
 {
   editor_state_t *state = new_editor_state();
@@ -40,6 +50,7 @@ int main(int argv, char *argc[])
 
   initscr();
   noecho();
+  render(state);
 
   while (!should_quit(state)) {
     const event_t event = getch();
@@ -58,14 +69,7 @@ int main(int argv, char *argc[])
 
 error_t update(const event_t event, editor_state_t *const state)
 {
-  switch (event) {
-  case 'q':
-    state->terminate = true;
-    break;
-  default:
-    return insert_character_at_point(state->point, event);
-  }
-  return SUCCESS;
+  return (state->mode->handler)(event, state);
 }
 
 
@@ -81,7 +85,7 @@ void render(const editor_state_t *const state)
 void render_modeline(const editor_state_t *const state)
 {
   attron(A_BOLD);
-  mvprintw(30, 0, "0:%d", column(state->point));
+  mvprintw(30, 0, "0:%d\t%s", column(state->point), state->mode->name);
   attroff(A_BOLD);
 }
 
@@ -102,6 +106,7 @@ editor_state_t* new_editor_state()
       state->buffer = buffer;
       state->point = buffer;
       state->terminate = false;
+      state->mode = &normal_mode;
     } else {
       destroy_buffer(buffer);
     }
@@ -115,4 +120,33 @@ void destroy_editor_state(editor_state_t *state)
   state->point = NULL;
   destroy_buffer(state->buffer);
   free(state);
+}
+
+
+error_t insert_mode_handler(event_t event, struct editor_state_t *const state)
+{
+  switch (event) {
+  case KEY_ESCAPE:
+    state->mode = &normal_mode;
+    break;
+  default:
+    return insert_character_at_point(state->point, event);
+  }
+  return SUCCESS;
+}
+
+
+error_t normal_mode_handler(event_t event, struct editor_state_t *const state)
+{
+  switch (event) {
+  case 'i':
+    state->mode = &insert_mode;
+    break;
+  case 'q':
+    state->terminate = true;
+    break;
+  default:
+    break;
+  }
+  return SUCCESS;
 }
